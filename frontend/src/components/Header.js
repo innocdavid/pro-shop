a244c483ca6e2bb3daf9df46ce84f607f0e714c7
Banja David Lotigo <innocdavid@gmail.com>
2022-08-31 12:35:18 +0300

    BOOTSTRAT COMPONENTS - Add react-bootstrap package - Add bootstrap components for the header - Add fontawesome icons

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+ const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark"  variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">proShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav style={{marginLeft: 'auto'}}>
+              <Nav.Link href="/cart"><i className="fas fa-shopping-cart" aria-hidden="true"></i>Cart</Nav.Link>
+              <Nav.Link href="/login"><i className="fas fa-user" aria-hidden="true"></i>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+ }
+
+ export default Header;
