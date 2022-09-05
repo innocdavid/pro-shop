0cdb5f78972603c4e92f1639a8fcbd3e11592883
Banja David Lotigo <innocdavid@gmail.com>
2022-09-05 13:33:58 +0300

    SEEDER TEST FILE TO INSERT AND DELETE PRODUCTS - Add a seeder.js file to insert and delete products - Create an importData function to insert products into mongodb - Create a destroyData function to delete products from mongodb -

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import connectDB from './config/db.js';
-import products from './data/productsArray.js';
 import colors from 'colors';
+import productRoute from './routes/productRoute.js';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import  express from 'express';
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => { 
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
-})
+app.use('/api/products', productRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
